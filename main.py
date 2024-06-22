5f03520ded78f5fb2ab051f914cda9d8c6e02fe6
bigrael123 <bigrael-x@hotmail.com>
2024-06-21 22:28:04 -0300

    commentary

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-sum_pos = 1 + 2 +3 +4 + 5
-average = (23 + 19 + 31) / 3
-how_many_times = 403/73
-excess = 403%73
+sum_pos = 1 + 2 +3 +4 + 5 #sum of the first 5 positive numbers
+average = (23 + 19 + 31) / 3 #average of 23+19+31
+how_many_times = 403/73 # how many times 73 is 403
+excess = 403%73 #excess of 703 divided by 73
 print(sum_pos)
 print(average)
-print(how_many_times)
+print(int(how_many_times))
 print(excess)
         
 
